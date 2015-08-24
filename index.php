22420c7d7e0776ce09ef1a9cf045366925a6d165
Patrick Barnum <patrick@hbrisk.com>
2015-08-23 22:29:53 -0700

    Adds testing grounds

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Content Box</title>
+    <link rel="stylesheet" type="text/css" href="/css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="/css/ContentBox.css" />
+</head>
+
+<body>
+Some text
+</body>
+
+<script type="text/javascript" src="/js/ContentBox.js"></script>
+<script>
+    var box = new ContentBox({
+        title: "This is a box!",
+        width: 400,
+        height: 300
+    });
+    //box.create();
+</script>
+
+</html>
